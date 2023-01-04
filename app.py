8935c50174c3db977fc9e3dfcba2457ed44edf11
edenable <121346056+edenable@users.noreply.github.com>
2023-01-04 11:20:21 +0900

    chart change (bar -> line) by github online

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-view = [100, 150, 30]
+view = [100, 150, 30, 40]
 
 st.write('# Youtube view')
 st.write('## raw')
 view 
 st.write('## bar chart')
-st.bar_chart(view)
+st.line_chart(view)
 import pandas as pd
 sview = pd.Series(view)
 sview
